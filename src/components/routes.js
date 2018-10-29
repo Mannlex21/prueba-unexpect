499be8fa02c5e6ad045f103eea2eb73ecdb22d46
mamurillo <mn.murillo@mavi.mx>
2018-10-29 10:19:08 -0600

    f2

--- src/components/routes.js
+++ src/components/routes.js
@@ -1,9 +1,11 @@
-import LigaWhatsapp from "./pages/liga/viewComponent.vue";
+
 import LigaWhatsappConsulta from "./pages/liga/viewComponent_Consult.vue";
 import Historial from "./pages/historial/viewComponent.vue";
 import Login from "./pages/login/viewComponent.vue";
+import hello from './HelloWorld.vue'
 const routes = [
-    { path: '/', component: LigaWhatsapp },
+    // { path: '/', component: LigaWhatsapp },
+    { path: '/', component: hello },
     { path: '/login', component: Login },
     { path: '/ligaConsulta', component: LigaWhatsappConsulta },
     { path: '/historial', component: Historial }
