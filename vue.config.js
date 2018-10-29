afc21df152ea84a231a607a885e72e1eaee07781
mamurillo <mn.murillo@mavi.mx>
2018-10-29 10:14:52 -0600

    f

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
+const path = require("path");
+
 module.exports = {
-  baseUrl: process.env.NODE_ENV === 'production'
-    ? '/prueba-unexpect/'
-    : '/',
-  baseUrl: './'
+    outputDir: path.resolve(__dirname, "./docs"),
+    baseUrl: './'
 }
